89c072f3688e5f959fee14dce35e709896ce27f1
Sansan <sansan@mail.com>
2018-05-24 23:24:43 +0300

    requestor and unit test requestor

---
+++ Requestor/Requestor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Net;
+using System.IO;
+using NLog;
+
+namespace RequestorLib
+{
+    public class Requestor
+    {
+        static Logger logger = LogManager.GetLogger("Requestor");
+        static Regex regRFDomen = new Regex(@"\.рф(\/|\?|$)", RegexOptions.IgnoreCase);
+        public string GetStrRespGetReq(string url, Dictionary<string, string> headers = null, Dictionary<string, string> cookies = null)
+        {
+            return MakeReq(url, headers: headers, cookies: cookies);
+        }
+        public string GetStrRespPostReq(string url, Dictionary<string, string> headers = null)
+        {
+            return "";
+        }
+        static string MakeReq(string url, string referer = "", string userAgent = "", string accept = "",
+            Dictionary<string, string> headers = null, Dictionary<string, string> cookies = null)
+        {
+            try
+            {
+                Uri uri = new Uri(url.StartsWith("http") ? url : $"http://{url}");
+                if (regRFDomen.IsMatch(url))
+                {
+                    IdnMapping idnMapping = new IdnMapping();
+                    //Uri uriCur = new Uri(url);
+                    UriBuilder uriBuilder = new UriBuilder(uri);
+                    uriBuilder.Host = idnMapping.GetAscii(uriBuilder.Host);
+                    uri = uriBuilder.Uri;
+                }
+                HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(uri);
+                if (headers != null && headers.Count > 0)
+                {
+                    foreach (var header in headers)
+                    {
+                        webRequest.Headers.Add(header.Key, header.Value);
+                    }
+                }
+                if(cookies != null && cookies.Count > 0)
+                {
+                    foreach (var cookie in cookies)
+                    {
+                        if (webRequest.CookieContainer == null)
+                            webRequest.CookieContainer = new CookieContainer();
+                        webRequest.CookieContainer.Add(new Cookie(cookie.Key, cookie.Value));
+                    }
+                }
+                //162.212.168.134:80 proxyhubsik hkftlim3dp
+                //webRequest.Proxy = new WebProxy("162.212.168.134", 80);
+                //webRequest.Proxy.Credentials = new NetworkCredential("proxyhubsik", "hkftlim3dp");
+                WebResponse webResponse = webRequest.GetResponse();
+                string strResp;
+                using (Stream stream = webResponse.GetResponseStream())
+                {
+                    using (StreamReader streamReader = new StreamReader(stream))
+                    {
+                        strResp = streamReader.ReadToEnd();
+                    }
+                }
+                webResponse.Close();
+                return strResp;
+            }
+            catch (Exception ex)
+            {
+                logger.Log(LogLevel.Error, ex.ToString());
+                return null;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RequestorLib;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Requestor requestor = new Requestor();
+            string urlMyScoreBasket = @"https://d.myscore.com.ua/x/feed/f_3_0_3_ru_1";
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            //headers.Add("referer", "https://d.myscore.com.ua/x/feed/proxy-local");
+            //headers.Add("user-agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36");
+            headers.Add("x-fsign", "SW9D1eZo");
+            headers.Add("x-referer", "https://www.myscore.com.ua/basketball/");
+            headers.Add("x-requested-with", "XMLHttpRequest");
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            cookies.Add("_ga", "GA1.3.269134732.1527160408");
+            cookies.Add("_gid", "GA1.3.471431654.1527160408");
+            cookies.Add("_gat_UA-821699-48", "1S");
+            string strResp = requestor.GetStrRespGetReq(urlMyScoreBasket, headers: headers, cookies: cookies);
+            string[] strSeporators = new string[] { "¬" };
+            strResp.Split(strSeporators, StringSplitOptions.None);
+        }
+    }
+}
